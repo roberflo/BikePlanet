90395348566028e448899625e695754efdfd4d44
roberto flores <robertoflores2790@gmail.com>
2019-02-16 13:48:55 -0600

    create product and search by all

---
+++ ApplicationLayer/Dto/Base/ProductDto.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GFStore.ApplicationLayer.Dto
+{
+    public class ProductDto
+    {
+        
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public int Likes { get; set; }
+
+    }
+}
---
+++ DataAccessLayer/Repositories/ProductRepository.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using GFShop.DataAccessLayer.Entities;
+using GFShop.DataAccessLayer.Repositories.Interfaces;
+
+namespace GFShop.DataAccessLayer.Repositories
+{
+    public class ProductRepository : RepositoryBase, IProductRepository
+    {
+        public ProductRepository(GFStoreContext Context) : base(Context)
+        {
+
+        }
+
+        public Product Create(Product product)
+        {
+            
+            _context.Products.Add(product);
+            _context.SaveChanges();
+
+            return product;
+        }
+
+        public void Delete(int Id)
+        {
+            var product = _context.Products.Find(Id);
+            if (product != null)
+            {
+                _context.Products.Remove(product);
+                _context.SaveChanges();
+            }
+        }
+
+        
+        public void Update(Product product)
+        {
+            _context.Products.Update(product);
+            _context.SaveChanges();
+        }
+
+        public IEnumerable<Product> GetAll()
+        {
+            return _context.Products;
+        }
+
+        public Product GetById(int id)
+        {
+             return _context.Products.Find(id);
+        }
+    }
+}
